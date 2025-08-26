a4bd14036a6f224d9b5de9b4a959f6604b0be57b
admin@windmill.dev <admin@windmill.dev>
2025-08-26 15:02:00 +0200

    [WM] Script 'u/admin/lighter_script' deployed

---
+++ u/admin/lighter_script.php
@@ -0,0 +1,18 @@
+<?php
+
+// remove the first // of the following lines to specify packages to install using composer
+// // require:
+// // monolog/monolog@3.6.0
+// // stripe/stripe-php
+
+function main(
+  // Postgresql $a,
+  // array $b,
+  // object $c,
+  int $d = 123,
+  string $e = "default value",
+  float $f = 3.5,
+  bool $g = true,
+) {
+  return $d;
+}
