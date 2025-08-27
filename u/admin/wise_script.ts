0186fcbaf71008b46253c2e361155bece233f92f
admin@windmill.dev <admin@windmill.dev>
2025-08-27 16:31:33 +0200

    [WM] Script 'u/admin/wise_script' deployed

--- u/admin/wise_script.ts
+++ u/admin/wise_script.ts
@@ -2,13 +2,13 @@
 // https://www.windmill.dev/docs/getting_started/scripts_quickstart/typescript#modes
 
 // import { toWords } from "number-to-words@1"
-// import * as wmill from "windmill-client"
+import * as wmill from "windmill-client"
 
 // fill the type, or use the +Resource type to get a type-safe reference to a resource
 // type Postgresql = object
 
 export async function rev() {
-  // console.log(wmill)
+  console.log(wmill)
   return "rev1";
 }
 
