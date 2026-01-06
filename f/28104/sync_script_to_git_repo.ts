0d389a85319b19bf60834d94b56e7d1f9594b45a
admin <admin@windmill.dev>
2026-01-06 20:33:04 +0100

    [WM] Script 'f/28104/sync_script_to_git_repo' deployed

--- f/28104/sync_script_to_git_repo.ts
+++ f/28104/sync_script_to_git_repo.ts
@@ -347,15 +347,10 @@ function composeCommitHeader(items: SyncObject[]): string {
     }
   }
 
-  let header = `[WM]:  ${parts.join("")}`;
+  let header = `[WM]: Deployed ${parts.join("")}`;
   if (othersCount > 0) {
     header += ` and ${othersCount} other object${othersCount > 1 ? "s" : ""}`;
   }
-
-  console.log(header);
-  console.log(header);
-
-  console.log(header);
 
   return header;
 }
