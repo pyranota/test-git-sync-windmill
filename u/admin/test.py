ecfdaeea31ba16f631e039c82b81bd545785cfad
Pyra <92104930+pyranota@users.noreply.github.com>
2026-03-06 12:31:27 +0100

    Update test.py

--- u/admin/test.py
+++ u/admin/test.py
@@ -4,6 +4,8 @@ import wmill
 # You can import any PyPi package. 
 # See here for more info: https://www.windmill.dev/docs/advanced/dependencies_in_python
 
+
+
 # you can use typed resources by doing a type alias to dict
 #postgresql = dict
 
